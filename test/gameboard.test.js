9ba252caa7520317c06ad9877357276134491228
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-25 18:05:44 +0300

    Added tests for Gameboard class for placing ship at a single square, record attacks, report hits/misses, check sunk

---
+++ test/gameboard.test.js
@@ -0,0 +1,40 @@
+import Gameboard from "../src/gameboard";
+import Ship from "../src/ship";
+
+describe("Gameboard", () => {
+  test("places ship at a given coordinate", () => {
+    const board = new Gameboard();
+    const ship = new Ship(2);
+    board.placeShip(ship, [0, 0]);
+    expect(board.ships.length).toBe(1);
+  });
+
+  test("register a hit on the ship", () => {
+    const board = new Gameboard();
+    const ship = new Ship(2);
+    board.placeShip(ship, [0, 0]);
+
+    const result = board.receiveAttack([0, 0]);
+    expect(result).toBe("hit");
+    expect(ship.hits).toBe(1);
+  });
+
+  test("records a missed attack", () => {
+    const board = new Gameboard();
+    const ship = new Ship(2);
+    board.placeShip(ship, [0, 0]);
+
+    const result = board.receiveAttack([1, 1]);
+    expect(result).toBe("missed");
+    expect(board.missedAttacks).toEqual([1, 1]);
+  });
+
+  test("reports all ships sunk", () => {
+    const board = new Gameboard();
+    const ship = new Ship(2);
+    board.placeShip(ship, [0, 0]);
+
+    board.receiveAttack([0, 0]);
+    expect(board.allSunk()).toBe(true);
+  });
+});
