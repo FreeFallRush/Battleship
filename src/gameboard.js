153f707a993a38974466d36f7bc24c9b8afc67cb
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-25 20:39:11 +0300

    Added class Gameboard with ship at a single square, record attacks, report hits/misses, check sunk

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,1 +1,28 @@
-export default class Gameboard {}
+export default class Gameboard {
+  constructor(size = 4) {
+    this.size = size;
+    this.ships = [];
+    this.missedAttacks = [];
+  }
+
+  placeShip(ship, coord) {
+    this.ships.push({ ship, coord, hits: [] });
+  }
+
+  receiveAttack(coord) {
+    for (let placed of this.ships) {
+      if (placed.coord[0] === coord[0] && placed.coord[1] === coord[1]) {
+        placed.ship.hit();
+        placed.hits.push(coord);
+        return "hit";
+      }
+    }
+
+    this.missedAttacks.push(coord);
+    return "missed";
+  }
+
+  allSunk() {
+    return this.ships.every((placed) => placed.ship.isSunk());
+  }
+}
