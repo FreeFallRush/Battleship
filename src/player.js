98bb2a050684670a57d4874101100627e1431b38
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-25 21:09:16 +0300

    Added tests for class Player to hold a gameboard and attack opponent board

--- src/player.js
+++ src/player.js
@@ -1,1 +1,12 @@
-export default class Player {}
+import Gameboard from "./gameboard";
+
+export default class Player {
+  constructor(name = "Player") {
+    this.name = name;
+    this.board = new Gameboard();
+  }
+
+  attack(enemy, coord) {
+    return enemy.board.receiveAttack(coord);
+  }
+}
