e56432cecc207fdb3aec7bb45caed76ca9de6671
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-26 14:23:14 +0300

    Added game.js to control game turns, attacks, and check for win/loss

---
+++ src/game.js
@@ -0,0 +1,40 @@
+import Player from "./player";
+import createFleet from "./fleet";
+import placeFleet from "./setupFleet";
+import { createBoard, updateCell, setStatus } from "./dom";
+
+const human = new Player("Human");
+const computer = new Player("Computer", true);
+
+const humanFleet = createFleet();
+placeFleet(human.board, humanFleet);
+
+function renderBoards() {
+  createBoard(human.board.ships, "player-board", false);
+  createBoard(computer.board.ships, "computer-board", true, handlePlayerClick);
+}
+
+function handlePlayerClick(coord, cell) {
+  const result = human.attack(computer.board, coord);
+  updateCell(cell, result);
+
+  if (computer.board.allSunk()) {
+    setStatus("You win!");
+    return;
+  }
+
+  const computerResult = computer.randomAttack(human.board);
+
+  const [row, col] = computer.attacks.at(-1);
+  const playerCells = document.getElementById("player-board").children;
+  const index = row * 10 + col;
+  updateCell(playerCells[index], computerResult);
+
+  if (human.board.allSunk()) {
+    setStatus("Computer wins!");
+    return;
+  }
+}
+
+renderBoards();
+setStatus("Game start! Click on computer board to attack!");
