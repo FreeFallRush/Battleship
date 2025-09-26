cd971500130b52b00244cee348d71728ad0e654b
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-26 14:53:32 +0300

    Deleted the console attacks to let game handle the board

--- src/index.js
+++ src/index.js
@@ -10,10 +10,3 @@ const ship2 = new Ship(3);
 
 player1.board.placeShip(ship1, [0, 0]);
 player2.board.placeShip(ship2, [1, 1]);
-
-console.log("Game start!");
-console.log("Player 1 attacks [1,1]:", player1.attack(player2, [1, 1]));
-console.log("Player 2 attacks [0,0]:", player2.attack(player1, [0, 0]));
-
-console.log("Are all Player 1 ships sunk?", player1.board.allSunk());
-console.log("Are all Player 2 ships sunk?", player2.board.allSunk());
