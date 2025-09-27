1bb9127185475cc59b6a1d70725b6eaeaabe6f3a
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-27 14:01:19 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import Ship from "./ship.js";
-import Player from "./player.js";
 import "./game.js";
-
-const player1 = new Player("Sally");
-const player2 = new Player("Robot");
-
-const ship1 = new Ship(2);
-const ship2 = new Ship(3);
-
-player1.board.placeShip(ship1, [0, 0]);
-player2.board.placeShip(ship2, [1, 1]);
